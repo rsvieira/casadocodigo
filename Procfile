4c2d9129ab8374e5185e203d2927ffc564343ade
Ramon Vieira <ramonsv89@gmail.com>
2016-08-31 21:49:55 -0300

    ajust heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -jar -D target/dependency/webapp-runner.jar --port $PORT target/*.war
--- src/main/java/br/com/casadocodigo/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/conf/JPAConfiguration.java
@@ -1,10 +1,14 @@
 package br.com.casadocodigo.conf;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -15,34 +19,80 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class JPAConfiguration {
 
+	@Autowired
+	private Environment environment; 
+	
 	@Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
 
         JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
 
         factoryBean.setJpaVendorAdapter(jpaVendorAdapter );
+        
+        // alterar a depender do banco
+        factoryBean.setDataSource(dataSourceConfigPostgreSQL());
 
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-
-        factoryBean.setDataSource(dataSource);
-
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        props.setProperty("hibernate.show_sql", "true");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-
-        factoryBean.setJpaProperties(props);
+        // alterar a depender do banco
+        factoryBean.setJpaProperties(getPropertiesPostgreSQL());
 
         factoryBean.setPackagesToScan("br.com.casadocodigo.model");
 
         return factoryBean;
 
     }
+
+	@SuppressWarnings("unused")
+	public DriverManagerDataSource dataSourceConfigMySql() {
+	
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		
+		return dataSource;
+	}
+	
+	public Properties getPropertiesMySql(){
+		
+		Properties props = new Properties();
+		
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		
+		return props; 
+	}
+	
+	public DriverManagerDataSource dataSourceConfigPostgreSQL() throws URISyntaxException {
+
+		// usuario:senha@host:port/path
+		URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+				
+		dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() + ":" + dbUrl.getPort() + dbUrl.getPath()); 
+		
+		dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
+		dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
+		
+		dataSource.setDriverClassName("com.postgresql.Driver");
+		
+		return dataSource;
+	}
+	
+	public Properties getPropertiesPostgreSQL() {
+		
+		Properties props = new Properties();
+		
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		
+		return props; 
+	}
 	
 	@Bean
 	public JpaTransactionManager transactionManager (EntityManagerFactory emf){
